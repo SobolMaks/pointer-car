118583990c8d8a04b5b5d1208dfba7b8a4ed5465
Max <sobolmax1984@mail.com>
2019-12-28 01:36:02 +0200

    add Car1

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        Car car = new Car();
+
+        car.drive(300);
+        car.reFuel(5);
+        car.total_mileage();
+
+
+
+
+
+
+
+        // write your code here
+    }
+
+}
